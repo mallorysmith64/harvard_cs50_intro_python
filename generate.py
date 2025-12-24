f8dd1198b229344a5f89cefb1c509f2e5a15033e
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-24 09:26:48 -0500

    shuffled cards

--- generate.py
+++ generate.py
@@ -1,8 +1,14 @@
 from random import choice
 from random import randint
+from random import shuffle
 
 coin = choice(["heads", "tails"])
 print("You flipped", coin)
 
 number = randint(1,10)
 print(number)
+
+cards = ["jack", "queen", "king"]
+shuffle(cards)
+for card in cards:
+    print(card)
