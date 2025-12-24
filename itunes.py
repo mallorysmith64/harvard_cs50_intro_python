f6f9499c62ec8a4c2c03ef8ca1c9a0438d87b096
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-24 11:01:54 -0500

    indent print json dumps to read json easier

--- itunes.py
+++ itunes.py
@@ -6,4 +6,4 @@ if len(sys.argv) != 2:
     sys.exit()
     
 response = requests.get("http://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json()))
+print(json.dumps(response.json(), indent=2))
