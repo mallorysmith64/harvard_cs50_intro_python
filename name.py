b98fd080fe8582af99e512d0f9f12792bd4c4109
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-25 11:22:13 -0500

    comment on slicing

--- name.py
+++ name.py
@@ -8,5 +8,5 @@ import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
     
-for arg in sys.argv[1:]:
+for arg in sys.argv[1:]: #[1:-1] would get rid of first and last
     print("Hello, my name is", arg)
