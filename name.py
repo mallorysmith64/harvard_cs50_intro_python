6b103096a099b679139411f9c58e38bbe475cdd7
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-25 11:20:36 -0500

    can get slice of argv with names

--- name.py
+++ name.py
@@ -1,6 +1,12 @@
 import sys
 
-try:
-    print("Hello, my name is", sys.argv[1])
-except IndexError:
-    print("Too few arguments")
+# try:
+#     print("Hello, my name is", sys.argv[1])
+# except IndexError:
+#     print("Too few arguments")
+    
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+    
+for arg in sys.argv[1:]:
+    print("Hello, my name is", arg)
