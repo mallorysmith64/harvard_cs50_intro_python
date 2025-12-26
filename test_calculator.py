650d220ecf2a03846451775f3b6c55143a2252b3
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-26 09:33:22 -0500

    broke tests up into multiple

--- test_calculator.py
+++ test_calculator.py
@@ -1,30 +1,20 @@
+import pytest
 from calculator import square
-
-
-def main():
-    test_square()
     
-def test_square():
-    try:
+def test_positive():
         assert square(2) == 4
-    except AssertionError:
-        print("2 squared was not 4")
-    try:
         assert square(3) == 9
-    except AssertionError:
-        print("3 squared was not 9")
-    try:
-        assert square(0) == 0
-    except AssertionError:
-        print("0 squared was not 0")
-    try:
+        
+def test_negative():
         assert square(-2) == 4
-    except AssertionError:
-        print("-2 squared was not 4")
-    try:
         assert square(-3) == 9
-    except AssertionError:
-        print("-3 squared was not 9")
 
-if __name__ == "__main__":
-    main()
+def test_zero():
+        assert square(0) == 0
+        
+def test_str():
+    with pytest.raises(TypeError):
+        square("cat")
+
+    if __name__ == "__main__":
+        pytest.main()
