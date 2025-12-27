0492dd91b05e0e4335d73213c293f0a5fa304418
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-27 11:45:37 -0500

    split names versus houses in students.csv

--- students.py
+++ students.py
@@ -1,4 +1,4 @@
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        print(f"{name[0]} is in {house[1]}")
+        print(f"{name} is in {house}")
