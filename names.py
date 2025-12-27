75529abb93098d70acae365bede0981164cb83fb
mallorysmith64 <mallorysmith64@gmail.com>
2025-12-27 11:31:32 -0500

    reading files sorted in reverse order

--- names.py
+++ names.py
@@ -1,4 +1,12 @@
 name = input("What's your name? ")
 
 with open("names.txt", "a") as file:
-    file.write(f"{name}\n")
+    file.write(f"{name}\n"     )
+    
+names = []
+with open("names.txt", "r") as file:
+    for line in file:
+        names.append(line.rstrip())
+        
+for name in sorted(names, reverse=True):
+    print(f"hello, {name}")
